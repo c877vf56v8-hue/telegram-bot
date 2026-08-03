4c64bd833c55d743ff3dba07de8e1489b1c34368
c877vf56v8-hue <c877vf56v8@privaterelay.appleid.com>
2026-08-03 23:30:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ if __name__ == '__main__':
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mesaj_yanitla))
 
     print("Bot sunucuda çalışıyor...")
-    aapp.run_polling(drop_pending_updates=True)
+    app.run_polling(drop_pending_updates=True)
 
